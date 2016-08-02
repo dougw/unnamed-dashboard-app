60acd24be2e99845cb1e55176d1751d66d61c68e
Dylan Steck <dylancsteck@gmail.com>
2016-08-02 15:59:00 -0400

    trying to get the first 5 titles

--- unnamed dashboard app/ViewControllers/TableViewPageController.swift
+++ unnamed dashboard app/ViewControllers/TableViewPageController.swift
@@ -7,18 +7,25 @@
 //
 import Foundation
 import UIKit
-//import Alamofire
-//import SwiftyJSON
+import Alamofire
+import SwiftyJSON
 import TTEventKit
 import ChameleonFramework
 
 class TableViewPageController: UIViewController{
+    var myArticles = [JSON]() ?? []
+//    var titlesString = [String]()
+    var myString = [JSON]() ?? []
+    var myVar = [JSON]() ?? []
     @IBOutlet weak var myCoolLabel: UILabel!
     var services = ["Calendar", "Google News", "Weather", "Social Feed" ]
 @IBOutlet weak var tableView: UITableView!
     
+    @IBOutlet weak var myTextView: UITextView!
     @IBOutlet weak var secondView: UIView!
     @IBOutlet weak var calendarNameView: UIView!
+    @IBOutlet weak var calendarNameLabel: UILabel!
+    @IBOutlet weak var titlesString: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         let colors:[UIColor] = [
@@ -27,6 +34,7 @@ class TableViewPageController: UIViewController{
         let background = GradientColor(.TopToBottom, frame: view.frame, colors: colors)
 //           view.backgroundColor = background
         view.backgroundColor = FlatSkyBlue()
+         self.calendarNameLabel.font = UIFont(name: "Montserrat-Regular", size: 15)!
 //        self.calendarNameView.layer.borderWidth = 2.5
 //        self.calendarNameView.layer.borderColor = UIColor(red:0.07, green:0.00, blue:0.00, alpha:1.0).CGColor
 //        secondView.backgroundColor = background
@@ -52,15 +60,49 @@ class TableViewPageController: UIViewController{
             print("You have 0 events on your calendar today.")
         }
     }
+    
+    override func viewDidAppear(animated: Bool) {
+        
+        let url = "https://newsapi.org/v1/articles"
+        let params = [ "source" : "googlenews" ,
+                       "sortBy" : "top" ,
+                       "apiKey" : "76bf0e6c09c846fcae1484659167aa91"]
+        myArticles = [JSON]()
+        Alamofire.request(.GET, url, parameters: params).responseJSON { response in
+            switch response.result {
+            case .Success(let data):
+                let json = JSON(data)
+                self.myArticles = json["articles"].arrayValue
+                var myVar = self.myArticles[0..<1]
+//                var firstFive = self.myArticles.stringValue[0..<5]
+//            myTextView.text = firstFive
+                for article in self.myVar {
+                    let title = article["title"].stringValue
+                    self.titlesString.text = title
+                    
+                }
+//                self.titlesString.reloadData()
+                //The titles are displayed on success
+            //                self.newsTextView.text = ("\(self.titlesString)\n\n")
+            case .Failure(let error):
+                //An error printed on failure
+                print("Could not connect \(error)")
+            }
+        }
+        
+        
+    }
+    
+
 }
 
 
-    
-    
-    
+
+
+
 //extension TableViewPageController: UITableViewDataSource, UITableViewDelegate {
 //     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-//        
+//
 //        return services.count
 //    }
 //    
